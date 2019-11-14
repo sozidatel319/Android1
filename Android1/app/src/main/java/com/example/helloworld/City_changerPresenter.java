d45bbd191f8ef8fbc64e1c925e327cabc9cb2f69
sozidatel319 <mbogat@yandex.ru>
2019-11-14 13:55:36 +0300

    Сделал управление из других активностей. Не сделал добавление смены темы

--- Android1/app/src/main/java/com/example/helloworld/City_changerPresenter.java
+++ Android1/app/src/main/java/com/example/helloworld/City_changerPresenter.java
@@ -2,7 +2,10 @@ package com.example.helloworld;
 
 public final class City_changerPresenter {
 
-    String cityName = "Moscow";
+    private String cityName = "";
+    private boolean infoisChecked;
+    private String wind;
+    private String pressure;
 
     private static City_changerPresenter instance;
 
@@ -17,5 +20,29 @@ public final class City_changerPresenter {
 
     public void setCityName(String cityName) {
         this.cityName = cityName;
+    }
+
+    public boolean getInfoisChecked() {
+        return infoisChecked;
+    }
+
+    public void setInfoisChecked(boolean infoisChecked) {
+        this.infoisChecked = infoisChecked;
+    }
+
+    public String getWind() {
+        return wind;
+    }
+
+    public void setWind(String wind) {
+        this.wind = wind;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(String pressure) {
+        this.pressure = pressure;
     }
 }
