da5f4e0feb825aeb17c51524a39257621ced97ab
sozidatel319 <mbogat@yandex.ru>
2020-06-05 19:23:34 +0300

    rename and move files

--- Android1/app/src/main/java/com/example/helloworld/WeatherProviderListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.helloworld;
-
-import com.example.helloworld.model.WeatherModel;
-import java.util.ArrayList;
-
-public interface WeatherProviderListener {
-
-    void updateWeather(WeatherModel model, ArrayList<String> time);
-}
--- Android1/app/src/main/java/com/example/helloworld/database/NoteAdapter.java
+++
@@ -1,5 +0,0 @@
-package com.example.helloworld.database;
-
-public class NoteAdapter {
-
-}
